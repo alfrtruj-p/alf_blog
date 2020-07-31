a4a7b987ef921a7d4fcf646593710ddcfb5315e1
Alfredo Trujillo <alfrtruj@protonmail.com>
2020-07-31 22:23:56 +0200

    adding html and css templates.

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
-
-
+posts = [
+    {
+        'author': 'Alfredo',
+        'title': 'First post',
+        'content': 'First post content',
+        'date_posted': '27 august 2020'
+    },
+    {
+        'author': 'Maria',
+        'title': 'Second post',
+        'content': 'Second post content',
+        'date_posted': '28 august 2020'
+    }
+]
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
 
